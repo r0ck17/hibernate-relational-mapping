b6660e7093f157c87e8a5984e611ec84ea824fad
Evgeniy <paut.evg@yandex.ru>
2023-10-11 12:41:19 +0300

    chore: remove logger from project

--- src/main/java/by/javaguru/util/HibernateUtil.java
+++ src/main/java/by/javaguru/util/HibernateUtil.java
@@ -5,19 +5,16 @@ import by.javaguru.entity.Student;
 import by.javaguru.entity.StudentProfile;
 import by.javaguru.entity.Trainer;
 import lombok.experimental.UtilityClass;
-import lombok.extern.slf4j.Slf4j;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
 
-@Slf4j
 @UtilityClass
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
 
     public static SessionFactory buildSessionFactory() {
         if (sessionFactory == null) {
-            log.info("Configure session factory");
             sessionFactory = new Configuration()
                     .configure()
                     .addAnnotatedClass(Course.class)
@@ -26,16 +23,12 @@ public class HibernateUtil {
                     .addAnnotatedClass(Trainer.class)
                     .setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy())
                     .buildSessionFactory();
-            log.info("Session factory configured");
         }
 
         return sessionFactory;
     }
 
     public static void closeSessionFactory() {
-        log.info("Trying to close session factory");
         sessionFactory.close();
-        log.info("Session factory closed");
     }
-
 }
