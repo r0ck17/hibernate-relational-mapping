63caabd1a3c39a9c42eb74f7d7730a64909a962e
Evgeniy <paut.evg@yandex.ru>
2023-10-11 12:39:18 +0300

    refactor(Course): remove builder annotation

--- src/main/java/by/javaguru/entity/Course.java
+++ src/main/java/by/javaguru/entity/Course.java
@@ -1,5 +1,6 @@
 package by.javaguru.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,8 +11,6 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -22,10 +21,8 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode(of = "name")
 @ToString(exclude = {"students", "trainers"})
-@Builder
 @Entity
 @Table(name = "course")
 public class Course {
@@ -36,13 +33,12 @@ public class Course {
     @Column(name = "name", unique = true, nullable = false)
     private String name;
 
-    @Builder.Default
-    @OneToMany(mappedBy = "course", orphanRemoval = true)
+    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "course", orphanRemoval = true)
     private List<Student> students = new ArrayList<>();
 
-    @Builder.Default
     @ManyToMany
-    @JoinTable(name = "courses_trainer", joinColumns = @JoinColumn(name = "course_id"),
+    @JoinTable(name = "courses_trainer",
+            joinColumns = @JoinColumn(name = "course_id"),
             inverseJoinColumns = @JoinColumn(name = "trainer_id"))
     private List<Trainer> trainers = new ArrayList<>();
 
